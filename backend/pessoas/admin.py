6cf49a3cb164f9c65d5078eb26bb60956cf2c30d
Hugo L. Ruffo <hlruffo@gmail.com>
2026-03-02 21:25:47 -0300

    feat: models/ registra model pessoa no admin

--- backend/pessoas/admin.py
+++ backend/pessoas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pessoa
 
-# Register your models here.
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'cpf', 'sexo', 'data_nasc')
+    search_fields = ('nome', 'cpf')
